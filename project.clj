8aa20dfd84addaeecbe7bef1c3ea87a0f2f4d080
Eric Lavigne <lavigne.eric@gmail.com>
2010-09-19 21:54:12 -0400

    Added clj-http library, acquired appid from Yahoo, and made my first successful request from the PlaceFinder API (saved as example in core.clj). The rate limit is 50k requests per day, across all users of this application - seems like more than enough.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject geo-dispersion "0.1" 
   :description "Calculate geographic dispersion index for personal networks" 
   :dependencies [[org.clojure/clojure "1.2.0"] 
-		 [org.clojure/clojure-contrib "1.2.0"]])
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [clj-http "0.1.1"]])
--- src/geo_dispersion/core.clj
+++ src/geo_dispersion/core.clj
@@ -1,1 +1,8 @@
-(ns geo-dispersion.core)
+(ns geo-dispersion.core
+  (:require [clj-http.client :as client]))
+
+; example of yahoo api request, returning json result
+(client/get "http://where.yahooapis.com/geocode"
+	    {:query-params 
+	     {"q" "1600+Pennsylvania+Avenue,+Washington,+DC", 
+	      "appid" "pItVa84o", "flags" "J"}})
