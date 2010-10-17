259627ae0b9a96e06796a997945a6688b9539846
Eric Lavigne <lavigne.eric@gmail.com>
2010-10-17 16:44:19 -0400

    Created parser for network data CSV, dividing the data into heading and row for each of the sections and trimming empty columns.

--- src/geo_dispersion/core.clj
+++ src/geo_dispersion/core.clj
@@ -44,12 +44,28 @@
                         "Which column contains location information?"
                         headings))
 
+(defn parse-network-data [text]
+  (let [in-matrix (parse-csv text)
+        empty-row #(or (empty? %) (every? empty? %))
+        non-empty-row (complement empty-row)
+        [[alter-head & alter-rows] after-alter-rows] (split-with non-empty-row in-matrix)
+        [alter-pair-head & alter-pair-rows] (filter non-empty-row after-alter-rows)
+        alter-head (filter not-empty alter-head)
+        alter-rows (map #(take (count alter-head) %) alter-rows)
+        alter-pair-head (filter not-empty alter-pair-head)
+        alter-pair-rows (map #(take (count alter-pair-head) %) alter-pair-rows)]
+    {:alter-head alter-head
+     :alter-rows alter-rows
+     :alter-pair-head alter-pair-head
+     :alter-pair-rows alter-pair-rows}))
+
 (defn main []
   (let [in-csv-file (manual-select-csv-to-open)]
     (when in-csv-file
-      (let [in-csv-matrix (parse-csv (slurp in-csv-file))
-            headings (first in-csv-matrix)]
-        (println (manual-select-location-field headings))))))
+      (let [parsed (parse-network-data (slurp in-csv-file))
+            alter-head (:alter-head parsed)
+            location-head (manual-select-location-field alter-head)]
+        (println location-head)))))
 
 ; See description of work in doc folder.
 
--- test/geo_dispersion/test_core.clj
+++ test/geo_dispersion/test_core.clj
@@ -8,15 +8,26 @@
 	 (:county (first (geosearch "Gainesville, FL"))))
       "Gainesville, FL is in Alachua County."))
 
+(def example-text (slurp "examples/small-disperse.csv"))
+(def example-alter-head
+     ["EgoID" "egocat" "egohome" "egodog"
+      "AlterID" "altercat" "alterdog" "alterhome" "alterfish"])
+
+(def example-alter-pair-head
+     ["EgoID" "Alter1ID" "Alter2ID" "know" "like"])
+
 (deftest csv-dependency
-  (let [text (slurp "examples/small-disperse.csv")
-        matrix (parse-csv text)
-        heading1 ["EgoID" "egocat" "egohome" "egodog"
-                  "AlterID" "altercat" "alterdog" "alterhome" "alterfish"]]
-    (is (= heading1 (first matrix))
+  (let [matrix (parse-csv example-text)]
+    (is (= example-alter-head (first matrix))
         "Heading matches expectation.")
     (is (every? empty? (nth matrix 11))
         "12th line is separator between alter and alter pair sections.")
     (is (= matrix (take (count matrix) (parse-csv (write-csv matrix))))
         "parse-csv and write-csv are inverses, except for a newline at the end.")))
 
+(deftest test-parse-network-data
+  (let [parsed (parse-network-data example-text)]
+    (is (= example-alter-head (:alter-head parsed))
+        "Alter heading matches expectation.")
+    (is (= example-alter-pair-head (:alter-pair-head parsed))
+        "Alter pair heading matches expectation")))
