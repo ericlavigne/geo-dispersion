0a49c67b9b6f721f1b4a6a7eb35bf93dc4a17bad
Eric Lavigne <lavigne.eric@gmail.com>
2010-12-05 23:55:47 -0500

    Sometimes a numeric code is given instead of a description of the location. Ask researcher to clarify what that code means.

--- src/geo_dispersion/core.clj
+++ src/geo_dispersion/core.clj
@@ -17,10 +17,11 @@
 			      "flags" "J"}}))
 
 (defn geosearch [address]
-  (let [raw-result (raw-geosearch address)]
-    (if (= 200 (:status raw-result))
-      (:Results (:ResultSet (json/read-json (:body raw-result))))
-      (throw (Exception. (str "Geosearch failed for address: " address))))))
+  (if (.matches address "[0-9]*") nil ; The researcher may disagree with Yahoo about what location the number "3" refers to.
+      (let [raw-result (raw-geosearch address)]
+        (if (= 200 (:status raw-result))
+          (:Results (:ResultSet (json/read-json (:body raw-result))))
+          (throw (Exception. (str "Geosearch failed for address: " address)))))))
 
 (defn manual-select-csv [show-dialog-method dialog-title]
   (let [fc (doto (JFileChooser.)
@@ -48,7 +49,8 @@
 (defn manual-select-string [title instructions string-list]
   (JOptionPane/showInputDialog
    nil instructions title JOptionPane/PLAIN_MESSAGE nil
-   (to-array string-list) (first string-list)))
+   (if (empty? string-list) nil (to-array string-list))
+   (first string-list)))
 
 (defn manual-select-location-field [headings for-who]
   (manual-select-string "Select location field"
@@ -70,20 +72,27 @@
      :alter-pair-head alter-pair-head
      :alter-pair-rows alter-pair-rows}))
 
-(defn location-for-alter-row [alter-row location-index location-chooser]
-  (let [locations (geosearch (nth alter-row location-index))]
-    (cond (empty? locations) nil
-          (empty? (rest locations)) (first locations)
-          :else (location-chooser locations))))
+(defn location-for-alter-row [alter-row location-index location-chooser location-clarifier]
+  (let [location-name (nth alter-row location-index)]
+    (if (empty? location-name) nil
+        (let [location-name (if (.matches location-name "[0-9]*")
+                              (location-clarifier location-name)
+                              location-name)]
+          (if (empty? location-name) nil
+              (let [locations (geosearch location-name)]
+                (cond (empty? locations) nil
+                      (empty? (rest locations)) (first locations)
+                      :else (location-chooser locations))))))))
 
 (defn index-of-item-in-seq [item items]
   (first (positions #(= item %) items)))
 
-(defn locations-for-alter-rows
-  [alter-head alter-rows location-head
-   {:keys [location-chooser] :or {location-chooser (fn [desc locs] (first locs))}}]
-  (let [location-index (index-of-item-in-seq location-head alter-head)]
-    (map (fn [row] (location-for-alter-row row location-index location-chooser))
+(defn locations-for-alter-rows [alter-head alter-rows location-head key-args]
+  (let [location-chooser (if (:location-chooser key-args) (:location-chooser key-args) (fn [desc locs] (first locs)))
+        location-clarifier (if (:location-clarifier key-args) (:location-clarifier key-args) (fn [loc-code] nil))
+        location-index (index-of-item-in-seq location-head alter-head)
+        _ (println "Used " location-head " to determine that index was " location-index)]
+    (map (fn [row] (location-for-alter-row row location-index location-chooser location-clarifier))
          alter-rows)))
 
 (defn show-location [location]
@@ -99,6 +108,15 @@
                                             " refer?")
                                        (map show-location locations))]
     (first (filter #(= selected (show-location %)) locations))))
+
+(defn manual-location-clarifier [for-who]
+  (memoize (fn [location-code]
+             (manual-select-string (str "Unable to understand " for-who " location - please clarify")
+                                   (str "Unable to understand " for-who " location: "
+                                        location-code
+                                        ". Please enter a new description of the location and press OK. "
+                                        "Or, to skip this location, press CANCEL.")
+                                   nil))))
 
 (defn degrees-to-radians
   "radians = degrees * 2pi / 360"
@@ -171,15 +189,23 @@
                                                                 ap-head ap-rows)
             ego-location-head (manual-select-location-field alter-head "ego")
             alter-location-head (manual-select-location-field alter-head "alter")
+            _ (println "Finding ego locations")
             ego-locations (locations-for-alter-rows alter-head alter-rows
                                                     ego-location-head
-                                                    manual-location-chooser)
+                                                    {:location-chooser manual-location-chooser
+                                                     :location-clarifier (manual-location-clarifier "ego")})
+            _ (println "Some ego locations:" (vec (take 3 ego-locations)))
+            _ (println "Finding alter locations")
             alter-locations (locations-for-alter-rows alter-head alter-rows
                                                       alter-location-head
-                                                      manual-location-chooser)
+                                                      {:location-chooser manual-location-chooser
+                                                       :location-clarifier (manual-location-clarifier "alter")})
+            _ (println "Some alter locations:" (vec (take 3 alter-locations)))
             ego-alter-distances (map distance ego-locations alter-locations)
+            _ (println "Some ego-alter distances:" (vec (take 3 ego-alter-distances)))
             ego-alter-to-alterloc (zipmap (map vector ego-ids alter-ids)
                                           alter-locations)
+            _ (println "Some alter locs:" (vec (take 3 ego-alter-to-alterloc)))
             ap-distances (map (fn [egoid alter1id alter2id]
                                 (apply distance
                                        (map (fn [alterid]
@@ -188,15 +214,19 @@
                               ap-ego-ids
                               ap-alter1-ids
                               ap-alter2-ids)
+            _ (println "Some ap-distances:" (vec (take 3 ap-distances)))
             ego-to-distances (group-by :ego (map (fn [ego distance] {:ego ego :distance distance})
                                                  (concat ego-ids ap-ego-ids)
                                                  (concat ego-alter-distances ap-distances)))
+            _ (println "Distances for an ego:" (first ego-to-distances))
             distinct-egos (distinct ego-ids)
+            _ (println "Number of distinct egos:" (count distinct-egos))
             ego-to-metrics (zipmap distinct-egos
                                    (map (fn [ego]
                                           (let [distances (filter identity (map :distance (ego-to-distances ego)))]
                                             {:geo-disp (dispersion-index distances) :geo-alt (dispersion-alt distances)}))
                                         distinct-egos))
+            _ (println "Some ego metrics:" (vec (take 3 ego-to-metrics)))
             num-alter-cols (apply max (map count-until-last-nonempty (cons alter-head alter-rows)))
             num-ap-cols (apply max (map count-until-last-nonempty (cons ap-head ap-rows)))
             new-alter-head (concat (take num-alter-cols alter-head)
